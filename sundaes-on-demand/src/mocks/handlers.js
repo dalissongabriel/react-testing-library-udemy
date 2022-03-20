d1f811e00edba3be883b23872b3da4d3967f9cc6
Alisson Gabriel <alisson.gabriel@assertivasolucoes.com.br>
2022-03-20 17:59:38 -0300

    added: setup msw

---
+++ sundaes-on-demand/src/mocks/handlers.js
@@ -0,0 +1,27 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          name: "Mint Chip",
+          imagePath: "/images/mint-chip.png",
+        },
+        {
+          name: "Vanilla",
+          imagePath: "/images/vanilla.png",
+        },
+        {
+          name: "Chocolate",
+          imagePath: "/images/chocolate.png",
+        },
+        {
+          name: "Salted caramel",
+          imagePath: "/images/salted-caramel.png",
+        },
+      ])
+    );
+  }),
+];
